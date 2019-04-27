51af3ed2996a137991ff62a408929705f555e16c
Gregory Hedrick <gregory@pixiitech.net>
2019-04-27 16:39:04 -0400

    Moved parallax view to shared

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,7 +2,7 @@ class ImagesController < ApplicationController
   def index
     if params[:search_keyword].present?
       session[:search_keyword] = params[:search_keyword]
-      @city_image_urls = Google::PlacePhotos.search(params[:search_keyword])
+      @city_image_urls = Google::PlacePhotos.search(params[:search_keyword], 750, 350)
       session[:city_image_urls] = @city_image_urls.to_json
     end
     @city_image_urls ||= JSON.parse(session[:city_image_urls] || '[]')
