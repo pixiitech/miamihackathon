f58edf16ccacfb335dbf3a0c3cd449785b09dc6d
Gregory Hedrick <gregory@pixiitech.net>
2019-04-27 11:16:42 -0400

    Added google places search

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 
 gem 'haml'
+gem 'faraday'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
@@ -51,6 +52,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'pry'
 end
 
 group :test do
---
+++ app/services/google/places.rb
@@ -0,0 +1,30 @@
+module Google
+  class Places
+    attr_accessor :keywords
+
+    def initialize(keywords)
+      @keywords = keywords
+    end
+
+    def self.search(keywords)
+      new(keywords).search
+    end
+
+    def endpoint_url
+      [
+        ENV['GOOGLE_PLACES_SEARCH_URL'],
+        'json?',
+        'key=',
+        ENV['GOOGLE_PLACES_API_KEY'],
+        '&inputtype=textquery',
+        '&input=',
+        keywords
+      ].join
+    end
+
+    def search
+      response = Faraday.get endpoint_url
+      JSON.parse(resp.body)['candidates'].first['place_id']
+    end
+  end
+end
