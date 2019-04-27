1d9da4f60c4de31ea1dc287771b419c6cbc4ccdf
Line 5 <noelhego@gmail.com>
2019-04-27 15:48:47 -0400

    event changes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
   def index
-    city = 'Miami' # session[:location] || 'Miami'
-    date = 7.days.from_now #session[:date] || 7.days.from_now
+    city = session[:location] || 'Miami'
+    date = session[:date] || 7.days.from_now
     @events = Eventful.search(location: city, date: date)
   end
 end
